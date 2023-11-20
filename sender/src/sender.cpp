f25a227e568c37057d5449eb1eaad88f8743bf46
imanshahmari <imanshahmari@gmail.com>
2023-11-20 16:04:19 +0100

    Fix verbose/use structs

--- sender/src/sender.cpp
+++ sender/src/sender.cpp
@@ -10,28 +10,36 @@
 #include "cluon-complete.hpp"
 #include "messages.hpp"
 
-#define INPUT_FILES "../../data/intrinsic-input/sample-camera/*.jpg"
+#define INPUT_FILES_INTRINSIC "../../data/intrinsic-input/sample-camera/*.jpg"
+#define INPUT_FILES_EXTRINSIC "../../data/extrinsic-input/*.jpg"
 
-void getCalibrationMatrix(cv::Mat& mtx, cv::Mat& dist, bool draw) {
+bool VERBOSE = false;
 
-    //TODO using this for subpix accuracy also so float accuracy?
+struct IntrinsicParameters {
+    cv::Mat cameraMatrix;
+    cv::Mat distortionCoefficients;
+};
+
+IntrinsicParameters getIntrinsicParameters() {
+
+    // TODO using this for subpix accuracy also so float accuracy?
     cv::TermCriteria criteria = cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::MAX_ITER, 30, 0.001);
 
-    int CHECKERBOARD[2]{6,9};
+    int CHECKERBOARD[2]{6, 9};
 
     std::vector<std::vector<cv::Point3f>> objpoints;
     std::vector<std::vector<cv::Point2f>> imgpoints;
 
-    std::vector<cv::Point3f> objp;  
-    for(int i{0}; i<CHECKERBOARD[1]; i++){
-        for(int j{0}; j<CHECKERBOARD[0]; j++)
-            objp.push_back(cv::Point3f(j,i,0));
+    std::vector<cv::Point3f> objp;
+    for (int i{0}; i < CHECKERBOARD[1]; i++) {
+        for (int j{0}; j < CHECKERBOARD[0]; j++)
+            objp.push_back(cv::Point3f(j, i, 0));
     }
 
     std::vector<cv::String> images;
-    cv::glob(INPUT_FILES, images);
+    cv::glob(INPUT_FILES_INTRINSIC, images);
 
-    for (const cv::String& fname : images) {
+    for (const cv::String &fname : images) {
         cv::Mat img = cv::imread(fname);
         cv::Mat gray;
         cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
@@ -40,11 +48,13 @@ void getCalibrationMatrix(cv::Mat& mtx, cv::Mat& dist, bool draw) {
         bool ret = cv::findChessboardCorners(gray, cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corners);
 
         if (ret) {
-            std::cout << fname << std::endl;
+            if(VERBOSE){
+                std::cout << fname << std::endl;
+            }
             objpoints.push_back(objp);
             imgpoints.push_back(corners);
 
-            if (draw) {
+            if (VERBOSE) {
                 cv::drawChessboardCorners(img, cv::Size(9, 6), corners, ret);
                 cv::resize(img, img, cv::Size(1152, 864));
                 cv::imshow("img", img);
@@ -55,9 +65,23 @@ void getCalibrationMatrix(cv::Mat& mtx, cv::Mat& dist, bool draw) {
     }
 
     cv::Mat img2 = cv::imread(images[0]);
-    cv::Mat rvecs, tvecs;
-    float ret = cv::calibrateCamera(objpoints, imgpoints, img2.size(), mtx, dist, rvecs, tvecs ,0 ,criteria);
+    cv::Mat mtx,dist,rvecs, tvecs;
+    float ret = cv::calibrateCamera(objpoints, imgpoints, img2.size(), mtx, dist, rvecs, tvecs, 0, criteria);
     std::cout << "RMSE:" << ret << std::endl;
+
+    if (VERBOSE) {
+        std::cout << mtx.row(0) << std::endl;
+        std::cout << mtx.row(1) << std::endl;
+        std::cout << mtx.row(2) << std::endl;
+        std::cout << dist.row(0) << std::endl;
+    }
+
+    // Create an instance of the IntrinsicParameters struct and assign the matrices
+    IntrinsicParameters result;
+    result.cameraMatrix = mtx.clone(); // Assuming mtx is the camera matrix
+    result.distortionCoefficients = dist.clone(); // Assuming dist is the distortion coefficients
+
+    return result;
 }
 
 void sendDataoverOD4(cluon::OD4Session &od4,cv::Mat &mtx, cv::Mat &dist, int id){
@@ -81,18 +105,20 @@ void sendDataoverOD4(cluon::OD4Session &od4,cv::Mat &mtx, cv::Mat &dist, int id)
 }
 
 int main(int argc, char *argv[]) {
-    bool draw = false;
 
     if (argc == 2 && std::string(argv[1]) == "--verbose") {
-        draw = true;
+        VERBOSE = true;
     }
 
-    cluon::OD4Session od4(132, [](cluon::data::Envelope &&envelope) noexcept {});
+    // cluon::OD4Session od4(132, [](cluon::data::Envelope &&envelope) noexcept {});
 
-    cv::Mat mtx, dist;
-    getCalibrationMatrix(mtx, dist, draw);
+    IntrinsicParameters parameters1;
+    parameters1 = getIntrinsicParameters();
 
-    sendDataoverOD4(od4, mtx, dist, 1);
+
+    cv::Mat mtx = parameters1.cameraMatrix;
+
+    // sendDataoverOD4(od4, mtx, dist, 1);
 
     return 0;
 }
